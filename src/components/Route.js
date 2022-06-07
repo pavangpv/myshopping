825d3835fb16814d2edc5c9d4fff24058165fe39
pavangpv <94555758+pavangpv@users.noreply.github.com>
2022-06-07 12:55:45 +0530

    Update Route.js

--- src/components/Route.js
+++ src/components/Route.js
@@ -28,12 +28,3 @@ export default function MyRoute() {
   );
 }
 
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-// function About() {
-//   return <h2>About</h2>;
-// }
-// function Users() {
-//   return <h2>Users</h2>;
-// }
